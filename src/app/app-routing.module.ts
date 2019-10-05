41e7b3c31936ca77853d8a6ac9a970a9996d264a
masayukinii1011 <masayukinii1011@gmail.com>
2019-10-05 13:45:47 +0900

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { Canvas01Component } from './canvas/canvas01/canvas01.component';
+import { Canvas02Component } from './canvas/canvas02/canvas02.component';
+import { Canvas03Component } from './canvas/canvas03/canvas03.component';
+import { Canvas04Component } from './canvas/canvas04/canvas04.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'canvas01', component: Canvas01Component },
+  { path: 'canvas02', component: Canvas02Component },
+  { path: 'canvas03', component: Canvas03Component },
+  { path: 'canvas04', component: Canvas04Component },
+  { path: '*', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { Canvas01Component } from './canvas/canvas01/canvas01.component';
+import { Canvas02Component } from './canvas/canvas02/canvas02.component';
+import { Canvas03Component } from './canvas/canvas03/canvas03.component';
+import { Canvas04Component } from './canvas/canvas04/canvas04.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    Canvas01Component,
+    Canvas02Component,
+    Canvas03Component,
+    Canvas04Component,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
